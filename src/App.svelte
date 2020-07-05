fbd025fbaf358aa1fd46eb44def6b0e9eaca7fc8
kingdaro <19603573+kingdaro@users.noreply.github.com>
2020-07-05 14:00:54 -0500

    .

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { css } from "@emotion/css";
+  import tw from "twin.macro";
+
+  const bigText = tw`text-xl`;
+</script>
+
+<h1 class={css(tw`text-purple-500`, bigText)}>Hello World</h1>
